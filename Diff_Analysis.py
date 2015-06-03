7007273b9392688498e89ac0b44af048be24f332
Oleg Guzey <oguzey@cybervisiontech.com>
2015-06-03 19:14:03 +0300

    Added sms4

--- Diff_Analysis.py
+++ Diff_Analysis.py
@@ -49,7 +49,8 @@ b5 = Variable(TypeVariable.UNKNOWN)
 b6 = Variable(TypeVariable.UNKNOWN)
 b7 = Variable(TypeVariable.UNKNOWN)
 b8 = Variable(TypeVariable.UNKNOWN)
-# b9 = Variable(TypeVariable.UNKNOWN)
+b9 = Variable(TypeVariable.UNKNOWN)
+b10 = Variable(TypeVariable.UNKNOWN)
 
 # ts1 = Transition(Side(a2, a3, a4), Side(a1, b1))
 # ts2 = Transition(Side(a3, a4, b1), Side(a2, b2))
@@ -60,19 +61,27 @@ b8 = Variable(TypeVariable.UNKNOWN)
 
 # system = SystemTransition(ts1, ts2, ts3, ts4, ts5, ts6)
 
-t1 = Transition(Side(a1), Side(a2, a3, b1))
-t2 = Transition(Side(a2), Side(a3, b1, b2))
-t3 = Transition(Side(a3), Side(b1, b2, g1))
-t4 = Transition(Side(b1), Side(b2, g1, g2))
-t5 = Transition(Side(b2), Side(g1, g2, g3))
+t1 = Transition(Side(a1), Side(a2, a3, a4, a5, b1))
+t2 = Transition(Side(a2), Side(a3, a4, a5, b1, b2))
+t3 = Transition(Side(a3), Side(a4, a5, b1, b2, b3))
+t4 = Transition(Side(a4), Side(a5, b1, b2, b3, b4))
+t5 = Transition(Side(a5), Side(b1, b2, b3, b4, b5))
+t6 = Transition(Side(b1), Side(b2, b3, b4, b5, b6))
+t7 = Transition(Side(b2), Side(b3, b4, b5, b6, b7))
+t8 = Transition(Side(b3), Side(b4, b5, b6, b7, b8))
+t9 = Transition(Side(b4), Side(b5, b6, b7, b8, g1))
+t10 = Transition(Side(b5), Side(b6, b7, b8, g1, g2))
+t11 = Transition(Side(b6), Side(b7, b8, g1, g2, g3))
+t12 = Transition(Side(b7), Side(b8, g1, g2, g3, g4))
+t13 = Transition(Side(b8), Side(g1, g2, g3, g4, g5))
 
 
-system = SystemTransition(t1, t2, t3, t4, t5)  # , t8, t9, t10, t11)
+system = SystemTransition(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13)
 print "Basic system is: \n" + str(system) + "\n\n"
 
 print "Creating common conditions..."
-input_conditions = CommonConditions(a1, a2, a3)
-output_conditions = CommonConditions(g1, g2, g3)
+input_conditions = CommonConditions(a1, a2, a3, a4, a5)
+output_conditions = CommonConditions(g1, g2, g3, g4, g5)
 
 print "generating ..."
 print "input conditions... " + input_conditions.generate_conditions()
@@ -164,6 +173,25 @@ for x in xrange(len(results)):
             print "NEW MAX " + str(max_est)
 
 print "\nMax estimation is " + str(max_est)
+
+res = max_est[1].split(' + ')
+while '' in res:
+    res.remove('')
+if len(res) > 1:
+    # print "[collect_estimates] res = " + str(res)
+    new_res = sorted(res, key=lambda est: int(est[2]), reverse=True)
+    # print "sorted " + str(new_res)
+    final_str = ''
+    while len(new_res) > 0:
+        elem = new_res.pop(0)
+        count = new_res.count(elem) + 1
+        final_str += " + %d*%s" % (count, elem)
+        while len(new_res) > 0 and new_res[0] == elem:
+            new_res.pop(0)
+
+    print "in siplify form = " + final_str[3:]
+else:
+    print ":("
 
 
 # def remove_empty(lst):
