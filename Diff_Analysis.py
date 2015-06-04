04d3d6a288c098002f11b693748ffcbb1feb3675
Oleg Guzey <oguzey@cybervisiontech.com>
2015-06-04 15:35:18 +0300

    Added def

--- Diff_Analysis.py
+++ Diff_Analysis.py
@@ -8,27 +8,6 @@ from conditions import CommonConditions
 from conditions import CustomConditions
 from test_for_res import get_normalise_result
 
-
-def get_estimation(est_data):
-    if est_data["fail"]:
-        return "Fail"
-
-    return "p^%d * p^(%d - %d) = p^%d" % (
-        est_data["transition_triviality"],
-        est_data["transition_with_unknowns"],
-        est_data["count_unknown_vars"],
-        est_data["transition_triviality"] +
-        est_data["transition_with_unknowns"] - est_data["count_unknown_vars"]
-    )
-
-
-def print_results(results):
-    for estimations in results:
-        print "\n\n"
-        for est in estimations:
-            print get_estimation(est)
-
-
 a1 = Variable(TypeVariable.INPUT)
 a2 = Variable(TypeVariable.INPUT)
 a3 = Variable(TypeVariable.INPUT)
@@ -52,19 +31,11 @@ b8 = Variable(TypeVariable.UNKNOWN)
 b9 = Variable(TypeVariable.UNKNOWN)
 b10 = Variable(TypeVariable.UNKNOWN)
 
-ts1 = Transition(Side(a2, a3), Side(b1, a1))
-ts2 = Transition(Side(a3, b1), Side(b2, a2))
-ts3 = Transition(Side(b1, b2), Side(b3, a3))
-ts4 = Transition(Side(b2, b3), Side(b4, b1))
-ts5 = Transition(Side(b3, b4), Side(b5, b2))
-ts6 = Transition(Side(b4, b5), Side(b6, b3))
-ts7 = Transition(Side(b5, b6), Side(b7, b4))
-ts8 = Transition(Side(b6, b7), Side(g1, b5))
-ts9 = Transition(Side(b7, g1), Side(g2, b6))
-ts10 = Transition(Side(g1, g2), Side(g3, b7))
 
-
-system = SystemTransition(ts1, ts2, ts3, ts4, ts5, ts6, ts7, ts8, ts9, ts10)
+ts1 = Transition(Side(a2, a3), Side(g1, a1))
+ts2 = Transition(Side(a3, g1), Side(g2, a2))
+ts3 = Transition(Side(g1, g2), Side(g3, a3))
+system = SystemTransition(ts1, ts2, ts3)
 
 # t1 = Transition(Side(a1), Side(a2, a3, a4, a5, b1))
 # t2 = Transition(Side(a2), Side(a3, a4, a5, b1, b2))
@@ -85,8 +56,8 @@ system = SystemTransition(ts1, ts2, ts3, ts4, ts5, ts6, ts7, ts8, ts9, ts10)
 print "Basic system is: \n" + str(system) + "\n\n"
 
 print "Creating common conditions..."
-input_conditions = CommonConditions(a1, a2, a3, a4)
-output_conditions = CommonConditions(g1, g2, g3, g4)
+input_conditions = CommonConditions(a1, a2, a3)
+output_conditions = CommonConditions(g1, g2, g3)
 
 print "generating ..."
 print "input conditions... " + input_conditions.generate_conditions()
@@ -182,140 +153,28 @@ for x in xrange(len(results)):
 
 print "\nMax estimation is " + str(max_est)
 
+final_str = ''
 res = max_est[1].split(' + ')
 while '' in res:
     res.remove('')
-if len(res) > 1:
+
+length = len(res)
+if length > 1:
     # print "[collect_estimates] res = " + str(res)
     new_res = sorted(res, key=lambda est: int(est[2]), reverse=True)
     # print "sorted " + str(new_res)
-    final_str = ''
     while len(new_res) > 0:
         elem = new_res.pop(0)
         count = new_res.count(elem) + 1
         final_str += " + %d*%s" % (count, elem)
         while len(new_res) > 0 and new_res[0] == elem:
             new_res.pop(0)
-
-    print "in siplify form = " + final_str[3:]
-else:
+    final_str = final_str[3:]
+    print "in siplify form = " + final_str
+elif length == 1:
+    final_str = res[0] if len(res) == 1 else 'MORE THAN ONE'
     print ":("
+else:
+    final_str = "emply list"
 
-
-# def remove_empty(lst):
-#     while '' in lst:
-#         lst.remove('')
-#     rm = []
-#     for x in xrange(len(lst)):
-#         if isinstance(lst[x], list) and len(lst[x]) == 0:
-#             rm.append(lst[x])
-#     for x in rm:
-#         lst.remove(x)
-
-#     for x in xrange(len(lst)):
-#         if isinstance(lst[x], list):
-#             remove_empty(lst[x])
-
-
-# def handle_list(lst):
-#     for x in xrange(len(lst)):
-#         if isinstance(lst[x], list):
-#             if len(lst[x]) > 1:
-#                 if all(isinstance(est, str) for est in lst[x]) and lst[x][0] != 'fork':
-#                     m = min([int(val[2]) for val in lst[x]])
-#                     lst[x] = 'p^%d' % m
-#                     print "replace to " + lst[x]
-#                 else:
-#                     handle_list(lst[x])
-#             if len(lst[x]) == 1:
-#                 if lst[x][0] == 'fork':
-#                     lst[x] = ''
-#                 else:
-#                     res = lst[x][0]
-#                     lst[x] = res
-#                     if isinstance(lst[x], list):
-#                         handle_list(lst[x])
-#         if isinstance(lst[x], list):
-#             # print "again list" + str(lst[x])
-#             handle_list(lst[x])
-
-
-# def handle_list_after_forks(lst):
-#     while any(isinstance(est, list) for est in lst):
-#         for x in lst:
-#             if isinstance(x, list):
-#                 lst.remove(x)
-#                 for y in x:
-#                     lst.append(y)
-#                 break
-
-
-# def replace_forks(lst):
-#     for x in xrange(len(lst)):
-#         if isinstance(lst[x], list):
-#             # assert lst[x][0] == 'fork'
-#             if not all(isinstance(elem, str) for elem in lst[x]):
-#                 replace_forks(lst[x])
-
-#             if all(isinstance(elem, str) for elem in lst[x]) and(
-#                     len(lst[x]) > 0 and lst[x][0] == 'fork'):
-#                 if len(lst[x]) == 1:
-#                     lst[x] = ''
-#                 else:
-#                     while '' in lst[x]:
-#                         lst[x].remove('')
-#                     fork = " + ".join(lst[x])
-#                     lst[x] = fork[7:]   # cut word 'fork'
-
-
-# def collect_estimates(lst):
-#     for x in xrange(len(lst)):
-#         estimate = lst[x]
-#         # print str(estimate)
-#         res = estimate.split(' + ')
-#         if len(res) > 1:
-#             # print "[collect_estimates] res = " + str(res)
-#             new_res = sorted(res, key=lambda est: int(est[2]), reverse=True)
-#             # print "sorted " + str(new_res)
-#             final_str = ''
-#             value = 0
-#             while len(new_res) > 0:
-#                 elem = new_res.pop(0)
-#                 count = new_res.count(elem) + 1
-#                 final_str += " + %d*%s" % (count, elem)
-#                 value += count * pow(0.5, int(elem[2]))
-#                 while len(new_res) > 0 and new_res[0] == elem:
-#                     new_res.pop(0)
-#             lst[x] = (final_str[3:], value)
-#         elif res[0] != "Fail":
-#             lst[x] = (res[0], pow(0.5, int(res[0][2])))
-#         else:
-#             lst[x] = (res[0], -1)
-
-
-# print "Total results is "
-# # pos, estimate, value
-# max_est = (0, 0, -1)
-# for x in xrange(len(results)):
-
-#     print "%d) before %s" % (x + 1, str(results[x]))
-#     handle_list(results[x])
-#     print "%d) before2 %s" % (x + 1, str(results[x]))
-#     remove_empty(results[x])
-#     print "%d) before3 %s" % (x + 1, str(results[x]))
-#     replace_forks(results[x])
-#     print "%d) before4 %s" % (x + 1, str(results[x]))
-#     remove_empty(results[x])
-#     print "%d) before5 %s" % (x + 1, str(results[x]))
-
-#     if isinstance(results[x][0], list):
-#         results[x] = results[x][0]
-#     handle_list_after_forks(results[x])
-#     print "%d) after %s" % (x + 1, str(results[x]))
-#     collect_estimates(results[x])
-#     print "%d) %s" % (x + 1, str(results[x]))
-#     for value in results[x]:
-#         if value[1] > max_est[2]:
-#             max_est = (x + 1, value[0], value[1])
-
-# print "\nMax estimation is " + str(max_est)
+print final_str
